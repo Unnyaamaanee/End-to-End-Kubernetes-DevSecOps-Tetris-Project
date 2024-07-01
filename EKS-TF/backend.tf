5f169f03ec1135d9425a57eaa754b50f2f303b6b
Jirayu Khunrak <136585173+Unnyaamaanee@users.noreply.github.com>
2024-07-01 10:28:03 +0700

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "jirayu-devsecops"
     region         = "ap-southeast-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "jirayu-dynamoDB"
+    dynamodb_table = "dynamoDB-jirayu"
     encrypt        = true
   }
   required_version = ">=0.13.0"
