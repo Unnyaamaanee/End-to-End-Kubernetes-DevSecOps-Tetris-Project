8ed0b92244b4f417526ce11bf95bfad911408239
Jirayu Khunrak <136585173+Unnyaamaanee@users.noreply.github.com>
2024-07-02 11:06:10 +0700

    Update vpc.tf

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -15,14 +15,14 @@ data "aws_internet_gateway" "igw" {
 data "aws_subnet" "subnet" {
   filter {
     name   = "tag:Subnet ID"
-    values = [subnet-0a8a0c8dca3737ca8]
+    values = "subnet-0a8a0c8dca3737ca8"
   }
 }
 
 data "aws_security_group" "sg-default" {
   filter {
     name   = "tag:Security group ID"
-    values = [sg-0ea6a494caf4f1a16]
+    values = "sg-0ea6a494caf4f1a16"
   }
 }
 
